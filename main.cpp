1324b268671901da7873f296e3c17831c1102386
XzoRit <xzorit@hotmail.com>
2017-03-08 20:22:19 +0100

    refactoring and formatting

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <boost/test/unit_test.hpp>
 #include <random>
 #include <map>
+#include <iomanip>
+#include <numeric>
+#include <algorithm>
 
 namespace utf = boost::unit_test;
 
@@ -13,7 +16,6 @@ public:
 
     ~dice_roller()
     {}
-
 };
 
 std::random_device rand_dev{};
@@ -21,40 +23,45 @@ std::mt19937 twister{rand_dev()};
 std::uniform_int_distribution<int> dist{1, 6};
 
 using roll_results = std::map<int, int>;
+const auto runs{100000};
+const auto scale{100};
 
-roll_results roll(int num_of_dices)
+const auto roll_one_dice = []()
 {
-    const int runs{1000000};
+    return dist(twister);
+};
+
+const auto roll = [](int num_of_dices)
+{
     roll_results rolls{};
 
     for(int i{}; i < runs; ++i)
     {
-        int sum{};
-        for(int j{}; j < num_of_dices; ++j)
-        {
-            sum += dist(twister);
-        }
+        std::vector<int> dice_rolls(num_of_dices);
+        std::generate(std::begin(dice_rolls), std::end(dice_rolls), roll_one_dice);
 
-        rolls[sum]++;
+        const auto sum_of_rolls =
+            std::accumulate(std::begin(dice_rolls), std::end(dice_rolls), 0);
+
+        rolls[sum_of_rolls]++;
     }
 
     return rolls;
-}
+};
 
 void print_roll_results(const roll_results& rolls)
 {
     for(const auto& p : rolls)
     {
-        BOOST_TEST_MESSAGE(p.first << " : " << std::string(p.second / 1000, '*'));
+        BOOST_TEST_MESSAGE(std::setw(2)
+            << p.first
+            << " : "
+            << std::string(p.second / scale, '*'));
     }
     BOOST_TEST_MESSAGE('\n');
 }
 
-BOOST_FIXTURE_TEST_CASE(roll_one_dice, dice_roller)
+BOOST_FIXTURE_TEST_CASE(roll_the_dices, dice_roller)
 {
-    print_roll_results(roll(1));
-    print_roll_results(roll(2));
-    print_roll_results(roll(3));
-    print_roll_results(roll(4));
-    print_roll_results(roll(5));
+    for(int i{1}; i <= 10; ++i) print_roll_results(roll(i));
 }
