d4f8f2251fd823624945d5bdacb118561821b881
XzoRit <xzorit@hotmail.com>
2017-03-08 14:48:32 +0100

    playground for std::random

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE random_test
+#include <boost/test/unit_test.hpp>
+#include <random>
+
+namespace utf = boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(test_1)
+{
+    BOOST_TEST(true);
+}
