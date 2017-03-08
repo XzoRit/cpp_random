89d9a26c405a16020214d45623646d519a0e5dc6
XzoRit <xzorit@hotmail.com>
2017-03-08 19:34:15 +0100

    extract dice rolling and printing in functions

--- main.cpp
+++ main.cpp
@@ -14,39 +14,47 @@ public:
     ~dice_roller()
     {}
 
-    std::random_device rand_dev{};
-    std::mt19937 twister{rand_dev()};
-    std::uniform_int_distribution<int> dist{1, 6};
 };
+
+std::random_device rand_dev{};
+std::mt19937 twister{rand_dev()};
+std::uniform_int_distribution<int> dist{1, 6};
+
+using roll_results = std::map<int, int>;
+
+roll_results roll(int num_of_dices)
+{
+    const int runs{1000000};
+    roll_results rolls{};
+
+    for(int i{}; i < runs; ++i)
+    {
+        int sum{};
+        for(int j{}; j < num_of_dices; ++j)
+        {
+            sum += dist(twister);
+        }
+
+        rolls[sum]++;
+    }
+
+    return rolls;
+}
+
+void print_roll_results(const roll_results& rolls)
+{
+    for(const auto& p : rolls)
+    {
+        BOOST_TEST_MESSAGE(p.first << " : " << std::string(p.second / 1000, '*'));
+    }
+    BOOST_TEST_MESSAGE('\n');
+}
 
 BOOST_FIXTURE_TEST_CASE(roll_one_dice, dice_roller)
 {
-    std::map<int, int> rolls{};
-
-    const int runs{1000000};
-    for(int i{}; i < runs; ++i)
-    {
-        rolls[dist(twister)]++;
-    }
-
-    for(const auto& p : rolls)
-    {
-        BOOST_TEST_MESSAGE(p.first << " : " << std::string(p.second / 1000, '*'));
-    }
-}
-
-BOOST_FIXTURE_TEST_CASE(test_roll_two_dices, dice_roller)
-{
-    std::map<int, int> rolls{};
-
-    const int runs{1000000};
-    for(int i{}; i < runs; ++i)
-    {
-        rolls[(dist(twister) + dist(twister))]++;
-    }
-
-    for(const auto& p : rolls)
-    {
-        BOOST_TEST_MESSAGE(p.first << " : " << std::string(p.second / 1000, '*'));
-    }
+    print_roll_results(roll(1));
+    print_roll_results(roll(2));
+    print_roll_results(roll(3));
+    print_roll_results(roll(4));
+    print_roll_results(roll(5));
 }
